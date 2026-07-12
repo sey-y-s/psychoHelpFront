e3a1634c63c178029458e9313237b967bf20d4f8
Dialla Coulibaly <diallacoulibaly@gmail.com>
2026-07-12 16:31:34 +0000

    add

--- src/app/pages/logins/logins.ts
+++ src/app/pages/logins/logins.ts
@@ -1,9 +1,47 @@
 import { Component } from "@angular/core";
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from "@angular/forms";
+import { RouterModule, Router, ActivatedRoute } from "@angular/router";
+import { MaterialModule } from "src/app/material.module";
+import { AuthService } from "../../core/services/auth.service";
+import { N } from "@angular/cdk/keycodes";
 
 @Component({
   selector: "app-logins",
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, MaterialModule, FormsModule, ReactiveFormsModule],
   templateUrl: "./logins.html",
   styleUrl: "./logins.css",
 })
-export class Logins {}
+export class Logins {
+  isLoading = false;
+  forms: FormGroup;
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private fb: FormBuilder,
+  ) {
+    this.forms = this.fb.group({
+      email: ["", [Validators.required, Validators.email]],
+      motDePasse: ["", Validators.required],
+    });
+  }
+
+  login() {
+    if (this.forms.invalid) return;
+    this.isLoading = true;
+    this.auth.login(this.forms.value).subscribe({
+      next: () => {
+        alert("vous êtes connectez avec succes");
+        this.router.navigate(["/dashboard"]);
+      },
+      error: () => (this.isLoading = false),
+    });
+  }
+}
