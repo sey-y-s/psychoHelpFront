9f001076f2b8a4059d4fb04755c6c4bb3815adce
Dialla Coulibaly <diallacoulibaly@gmail.com>
2026-07-12 16:21:23 +0000

    ajout des modif

---
+++ src/app/pages/logins/logins.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-logins",
+  imports: [],
+  templateUrl: "./logins.html",
+  styleUrl: "./logins.css",
+})
+export class Logins {}
