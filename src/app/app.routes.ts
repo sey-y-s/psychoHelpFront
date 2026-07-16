dcd18ec8a8965bc3602532e69ee0fcbcc6df248e
DIARRA Adama <ad5299019@gmail.com>
2026-07-16 11:45:33 +0000

    merging with my version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -50,10 +50,10 @@ export const routes: Routes = [
  //    path: 'psychologues',
  //    loadComponent: () => import('./pages/psychologue-list/psychologue-list.component').then(m => m.PsychologueListComponent)
  //  },
- //  {
- //    path: 'psychologues/:id',
- //    loadComponent: () => import('./pages/psychologue-detail/psychologue-detail.component').then(m => m.PsychologueDetailComponent)
- //  },
+  {
+    path: 'psychologues/:id',
+    loadComponent: () => import('./pages/psychologue-detail/psychologue-detail').then(m => m.PsychologueDetail)
+  },
  //
  //  // Rendez-vous (connecté)
  //  {
