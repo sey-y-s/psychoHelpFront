d099bcffd07254bc66a9a6d737553cdd2fa4395a
Idrissa Gassama <idrissag97@gmail.com>
2026-07-21 11:02:52 +0000

    page 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -150,11 +150,15 @@ export const routes: Routes = [
       }
     ]
   },
-
+  {
+    path: '404',
+    loadComponent: () =>
+        import('./shared/page-not-found/page-not-found').then(m => m.PageNotFound)
+  },
   // Redirection par défaut si la route n'existe pas
   // TODO : Page 404
   {
     path: '**',
-    redirectTo: ''
+    redirectTo: '404'
   }
 ];
