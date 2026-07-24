73a05871046d6fc7a313053567df491b32213002
MoussaTraore <moutra213@gmail.com>
2026-07-24 15:38:24 +0000

    correction

---
+++ src/app/shared/components/pipes/statut-conseil-pipe-pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+  name: "statutConseilPipe",
+})
+export class StatutConseilPipePipe implements PipeTransform {
+  transform(value: string): string {
+    if(value=="ENATTENTE"){
+         return "En attente"
+    }else if(value==="VALIDER"){
+      return "validé"
+    }
+    else{
+      return "refusé"
+    }
+   
+  }
+}
