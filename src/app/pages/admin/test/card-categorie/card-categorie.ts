055ff241d7117ae82a1559a739d19b661a89d5d5
med <seibacly@gmail.com>
2026-07-27 18:07:04 +0000

    gerer categorie test

---
+++ src/app/pages/admin/test/card-categorie/card-categorie.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-card-categorie",
+  imports: [],
+  templateUrl: "./card-categorie.html",
+  styleUrl: "./card-categorie.css",
+})
+export class CardCategorie {}
--- src/app/pages/admin/test/test.ts
+++ src/app/pages/admin/test/test.ts
@@ -1,5 +1,9 @@
-import { Component } from "@angular/core";
+import {ChangeDetectorRef, Component, OnInit} from "@angular/core";
 import {RouterLink, RouterModule} from "@angular/router";
+import {TestService} from "../../../core/services/test.service";
+import {CategorieTestService} from "../../../core/services/categorie-test.service";
+import {Conseil} from "../../../models/conseil.model";
+import {categorieTest} from "../../../models/categorie-test";
 
 @Component({
   selector: "app-test",
@@ -10,6 +14,32 @@ import {RouterLink, RouterModule} from "@angular/router";
   templateUrl: "./test.html",
   styleUrl: "./test.css",
 })
-export class Test {
+export class Test implements OnInit {
+
+  constructor(
+      private categorieTest: CategorieTestService,
+      private cdRef: ChangeDetectorRef
+  ) {
+  }
+  listCategorie: categorieTest[] = []
+
+
+
+  ngOnInit() {
+    this.getCategories()
+  }
+
+  getCategories() {
+    this.categorieTest.getCategories().subscribe({
+      next: data => {
+        this.listCategorie = data;
+        console.log(data);
+        this.cdRef.detectChanges();
+      },
+      error: (err) => {
+        console.error("Erreur lors de la récupération des categories :", err);
+      }
+    })
+  }
 
 }
