2cc1ebdd180f229d0cd93c9654a79b028c43a68a
Seyba Yacoub <147198884+sey-y-s@users.noreply.github.com>
2026-07-27 18:10:34 +0000

    Delete src/app/pages/admin/test/card-categorie/card-categorie.ts

--- src/app/pages/admin/test/card-categorie/card-categorie.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-card-categorie",
-  imports: [],
-  templateUrl: "./card-categorie.html",
-  styleUrl: "./card-categorie.css",
-})
-export class CardCategorie {}
