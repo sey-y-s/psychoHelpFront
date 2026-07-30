98603f3efc34a7f253372e9f5ab08f3abe6f1b9b
Seyba Yacoub <147198884+sey-y-s@users.noreply.github.com>
2026-07-30 14:18:25 +0000

    Delete src/app/layout/citoyen/pages/notifications/notification-cards/notification-cards.ts

--- src/app/layout/citoyen/pages/notifications/notification-cards/notification-cards.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-notification-cards",
-  imports: [],
-  templateUrl: "./notification-cards.html",
-  styleUrl: "./notification-cards.css",
-})
-export class NotificationCards {}
