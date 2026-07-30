f597e5f417c2a5ae20c4ce046c140e16e781fa0b
Seyba Yacoub <147198884+sey-y-s@users.noreply.github.com>
2026-07-30 14:28:52 +0000

    Delete src/app/layout/citoyen/pages/notifications/notification-liste/notification-liste.ts

--- src/app/layout/citoyen/pages/notifications/notification-liste/notification-liste.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-notification-liste",
-  imports: [],
-  templateUrl: "./notification-liste.html",
-  styleUrl: "./notification-liste.css",
-})
-export class NotificationListe {}
