5c3fb5a3b81165679bd18b3b5dc5320e5caa7806
moussa Traore <moutra213@gmail.com>
2026-07-31 11:29:16 +0200

    amelioration

--- src/app/pages/citoyen/rdv/rdv.ts
+++ src/app/pages/citoyen/rdv/rdv.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectorRef, Component, OnInit, Signal, inject, signal } from "@angular/core";
+import {
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  Signal,
+  inject,
+  signal,
+} from "@angular/core";
 import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
 import { CreneauService } from "../../../core/services/creneau.service";
 import { SeanceService } from "../../../core/services/seance.service";
@@ -8,69 +15,74 @@ import { ActivatedRoute, RouterLink } from "@angular/router";
 import { MatSnackBar } from "@angular/material/snack-bar";
 import { Filtre } from "./filtre/filtre";
 import { NotificationService } from "../../../core/services/notification.service";
-
+import { DatePipe } from "@angular/common";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: "app-rdv",
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink,Filtre],
+  imports: [ReactiveFormsModule, RouterLink, Filtre,DatePipe,MatIconModule],
   templateUrl: "./rdv.html",
   styleUrl: "./rdv.css",
 })
-export class Rdv{
+export class Rdv {
   private creneauService = inject(CreneauService);
   private seanceService = inject(SeanceService);
   private formBuilder = inject(FormBuilder);
-  private cdr = inject(ChangeDetectorRef);
-  private route=inject(ActivatedRoute)
-   psyId!:number
+  private route = inject(ActivatedRoute);
+  psyId!: number;
   message: string | null = null;
-  messageSnackBar=inject(NotificationService)
-   creneauDisponiblePourcitoyenfiltre=signal<CreneauInterfaceResponse2[]>([]);
-  //  si le citoyen a choisi une date
-  dateSelectionnee = false;
+  messageSnackBar = inject(NotificationService);
+  creneauDisponiblePourcitoyenfiltre = signal<CreneauInterfaceResponse2[]>([]);
+  aujourdHui = new Date().toISOString().split("T")[0];
+  dateSelectionnee = signal(
+    this.aujourdHui
+  );
 
   form2 = this.formBuilder.group({
-    creneauId: this.formBuilder.control<number | null>(null,Validators.required,),
+    creneauId: this.formBuilder.control<number | null>(
+      null,
+      Validators.required,
+    ),
 
     dateRdv: this.formBuilder.control<string | null>(null, Validators.required),
   });
 
   constructor() {
-    this.chargerDisponibilites("");
+    console.log(this.aujourdHui);
+    this.chargerDisponibilites(this.aujourdHui);
   }
 
-  chargerDisponibilites(datepris:string) {
-           this.psyId=+this.route.snapshot.paramMap.get('id')!
-                   console.log(this.psyId)
+  chargerDisponibilites(datepris: string) {
+      this.dateSelectionnee.set(datepris);
 
-              this.creneauService.listerDesCreneauxDisponiblePourCitoyen(this.psyId).subscribe({
-                    next: (donnees) => {
-                      if(datepris===""){
-                                                this.dateSelectionnee=true
+    this.psyId = +this.route.snapshot.paramMap.get("id")!;
+    console.log(this.psyId);
 
-                             this.creneauDisponiblePourcitoyenfiltre.set(donnees);
-                             console.log(this.creneauDisponiblePourcitoyenfiltre())
+    this.creneauService
+      .listerDesCreneauxDisponiblePourCitoyen(this.psyId)
+      .subscribe({
+        next: (donnees) => {
+          if (datepris === this.aujourdHui) {
+            this.creneauDisponiblePourcitoyenfiltre.set(
+              donnees.filter((donnee) => donnee.date === this.aujourdHui),
+            );
+          } else {
+            this.creneauDisponiblePourcitoyenfiltre.set(
+              donnees.filter((donnee) => donnee.date === datepris),
+            );
+            //console.log(this.creneauDisponiblePourcitoyenfiltre())
+          }
+        },
 
-                      }else{
-                        this.dateSelectionnee=true
-                           this.creneauDisponiblePourcitoyenfiltre.set(
-                                 donnees.filter((donnee)=>donnee.date===datepris)
-                           )
-                           //console.log(this.creneauDisponiblePourcitoyenfiltre())
-                      }
-              
-                    },
-
-                    error: (error) => {
-                      console.log(error);
-                    },
-                  });
-        
-    
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
   onSubmit2(creneau: CreneauInterfaceResponse2) {
-    this.form2.patchValue({
+    if(confirm("voulez-vous vraiment prendre ce rendez vous ?")){
+      this.form2.patchValue({
       creneauId: creneau.creneauId,
 
       dateRdv: creneau.date,
@@ -84,13 +96,17 @@ export class Rdv{
         next: (response) => {
           console.log(response);
           this.chargerDisponibilites(creneau.date);
-          this.messageSnackBar.succes("rendez vous reservé avec succes!")
+          this.messageSnackBar.succes("rendez vous reservé avec succes!");
         },
 
         error: ({ error }) => {
-          this.messageSnackBar.erreur("Erreur lors de la prise du rendez-vous!")
+          this.messageSnackBar.erreur(
+            "Erreur lors de la prise du rendez-vous!",
+          );
         },
-      });
+      })
+
+    }
+    
   }
- 
 }
